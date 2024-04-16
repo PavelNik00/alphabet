99b371fa12c399db304fa788fdef52ae70ca423e
PavelNik00 <gh9.111@yandex.ru>
2024-04-16 12:08:06 +0200

    Initial Commit

---
+++ Alphabet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alphabet
+//
+//  Created by Pavel Nikipelov on 16.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
